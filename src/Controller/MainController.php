42e8de869210aa3f6581f2ac785643abeee51d6d
jauclair <jauclair71@gmail.com>
2019-06-21 14:09:51 +0200

    page contact fonctionnel et incoporation du recaptcha google

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use App\Entity\Accommodation;
+use App\Entity\Admin;
+use App\Entity\Client;
+use App\Entity\Country;
+use App\Entity\Flight;
+use App\Entity\Order;
 use App\Entity\Tour;
+use \Swift_Mailer;
+use \Swift_Message;
+use App\Service\Recaptcha;
 
 
 class MainController extends AbstractController{
@@ -29,7 +38,7 @@ class MainController extends AbstractController{
     }
 
     /**
-    * @Route("choisissez-votre-pays", name="travel-list")
+    * @Route("choisissez-votre-voyage", name="travel-list")
     * Page d'accueil du site
     */
     public function travelList(){
@@ -41,8 +50,53 @@ class MainController extends AbstractController{
     * @Route("contactez-nous", name="contact")
     * Page du contact du site
     */
-    public function contact(){
+    public function contact(Request $request, Swift_Mailer $mailer, Recaptcha $recaptcha){
+        
 
+        if($request->isMethod('POST')){
+
+            //recuperation des données POST
+            $email = $request->request->get('email');
+            $subject = $request->request->get('subject');
+            $content = $request->request->get('message');
+            $recaptchaCode = $request->request->get('g-recaptcha-response');
+
+            //bloc des verification des champs
+            if(!filter_var($email , FILTER_VALIDATE_EMAIL)){
+                $errors['emailInvalid'] = true;
+            }
+            if(!preg_match('#^.{5,120}$#', $subject)){
+                $errors['subjectInvalid']=true;
+            }
+            if(!preg_match('#^.{5,4000}$#', $content)){
+                $errors['messageInvalid']=true; 
+            }
+            if(!$recaptcha->isValid($recaptchaCode, $request->server->get('REMOTE_ADDR'))){
+                $errors['captchaInvalid'] = true;
+            }
+            //Si des erreurs ont été commises, alors retour de la page de contact avec le tableau des erreurs.
+            if(isset($errors)){
+                return $this->render('contact.html.twig', array('errorsList'=>$errors));
+            }else{
+                //Si il n'y a pas d'erreurs, alors envoi du mail à l'adresse mail du site... 
+                $message = (new Swift_Message('Formulaire de contact venant du site'))
+                    ->setFrom($email)
+                    ->setTo('monsite@gmail.com')
+                    ->setBody(
+                        $this->renderView('emails/contact-email.html.twig',['content'=>$content, 'subject'=>$subject]),
+                        'text/html'
+                    )
+                    ->addPart(
+                        $this->renderView('emails/contact-email.txt.twig', ['content'=>$content , 'subject'=>$subject]),
+                        'text/plain'
+                    )
+                ;
+                $mailer->send($message);
+                //retour de la page contact avec le tableau contenant le message de succès d'envoi de mail
+                return $this->render('contact.html.twig', array('success' => true));
+            }  
+        }
+        
         return $this->render('contact.html.twig');
     }
 

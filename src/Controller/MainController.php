335c66db609d2ea69c94b9b0832919d1bda1907f
jauclair <jauclair71@gmail.com>
2019-06-25 13:12:07 +0200

    Ajout d'un accusé de reception pour le mail de contact

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,7 +19,7 @@ use App\Service\Recaptcha;
 
 class MainController extends AbstractController{
 /**
-* @Route("/accueil/", name="home")
+* @Route("/", name="home")
 * Page d'accueil du site
 */
 public function home(){
@@ -37,81 +37,81 @@ public function customTravel(Request $request, Swift_Mailer $mailer, Recaptcha $
     //Si le formulaire a été cliqué
     if($request->isMethod('POST')){
     
-    //récuperation des données POST du formulaire
-    $email = $request->request->get('email');
-    $country = $request->request->get('country');
-    $departureDate = $request->request->get('departureDate');
-    $returnDate = $request->request->get('returnDate');
-    $groupNbr = $request->request->get('groupNbr');
-    $minPrice = $request->request->get('minPrice');
-    $maxPrice = $request->request->get('maxPrice');
-    $message = $request->request->get('message');
-    $recaptchaCode = $request->request->get('g-recaptcha-response');
-    
-        //Bloc des vérifs
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            $errors['invalidEmail'] = true;
-        }
+        //récuperation des données POST du formulaire
+        $email = $request->request->get('email');
+        $country = $request->request->get('country');
+        $departureDate = $request->request->get('departureDate');
+        $returnDate = $request->request->get('returnDate');
+        $groupNbr = $request->request->get('groupNbr');
+        $minPrice = $request->request->get('minPrice');
+        $maxPrice = $request->request->get('maxPrice');
+        $message = $request->request->get('message');
+        $recaptchaCode = $request->request->get('g-recaptcha-response');
         
-        if(empty($country)){
-            $errors['invalidCountry'] = true;
-        }
+            //Bloc des vérifs
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors['invalidEmail'] = true;
+            }
+            
+            if(empty($country)){
+                $errors['invalidCountry'] = true;
+            }
 
-        if(empty($departureDate)){
-            $errors['invalidDepartureDate'] = true;
-        }
+            if(empty($departureDate)){
+                $errors['invalidDepartureDate'] = true;
+            }
 
-        if(empty($returnDate)){
-            $errors['invalidReturnDate'] = true;
-        }
+            if(empty($returnDate)){
+                $errors['invalidReturnDate'] = true;
+            }
 
-        if(empty($groupNbr)){
-            $errors['invalidGroupNbr'] = true;
-        }
-        
-        if(!preg_match('#^[0-9]+(\.[0-9]{2})?$#', $minPrice)){
-            $errors['invalidMinPrice'] = true;
-        }
+            if(empty($groupNbr)){
+                $errors['invalidGroupNbr'] = true;
+            }
+            
+            if(!preg_match('#^[0-9]+(\.[0-9]{2})?$#', $minPrice)){
+                $errors['invalidMinPrice'] = true;
+            }
 
-        if(!preg_match('#^\d{2,10}(?:\.\d{1,4})?$#', $maxPrice)){
-            $errors['invalidMaxPrice'] = true;
-        }
+            if(!preg_match('#^\d{2,10}(?:\.\d{1,4})?$#', $maxPrice)){
+                $errors['invalidMaxPrice'] = true;
+            }
 
-        if(!preg_match('#^.{0,8000}$#', $message)){
-            $errors['invalideMessage'] = true;
-        }
+            if(!preg_match('#^.{0,8000}$#', $message)){
+                $errors['invalideMessage'] = true;
+            }
 
-        if(!$recaptcha->isValid($recaptchaCode, $request->server->get('REMOTE_ADDR'))){
-            $errors['captchaInvalid'] = true;
-        }
-        
-        //Si erreur
-        if(isset($errors)){
-            //retour a la page custom-travel avec les erreurs 
-            return $this->render('custom-travel.html.twig', array('errorsList' => $errors));
-        
-        //Sinon envoie du msg
-        }else{    
-            //Création du msg de la demande de séjour 
-            //msg avec HTML
-            $message = (new Swift_Message('Demande de séjour personalisé'))
-                ->setFrom($email)
-                ->setTo('admin@monsite.com')
-                ->setBody(
-                    $this->renderView('emails/custom-email.html.twig', ['country' => $country,'departureDate' => $departureDate,'returnDate' => $returnDate,'groupNbr' => $groupNbr,'minPrice' => $minPrice,'maxPrice' => $maxPrice,'message' => $message] ),
-                    'text/html'
-                )
-                //msg TXT
-                ->addPart(
-                    $this->renderView('emails/custom-email.txt.twig', ['country' => $country,'departureDate' => $departureDate,'returnDate' => $returnDate,'groupNbr' => $groupNbr,'minPrice' => $minPrice,'maxPrice' => $maxPrice,'message' => $message]),
-                    'text/plain'
-                )
-            ;
-            //Envoie du msg a l'admin
-            $mailer->send($message);
-            //retour de la page avec msg de succès
-            return $this->render('custom-travel.html.twig', array('success' => true));
-        }
+            if(!$recaptcha->isValid($recaptchaCode, $request->server->get('REMOTE_ADDR'))){
+                $errors['captchaInvalid'] = true;
+            }
+            
+            //Si erreur
+            if(isset($errors)){
+                //retour a la page custom-travel avec les erreurs 
+                return $this->render('custom-travel.html.twig', array('errorsList' => $errors));
+            
+            //Sinon envoie du msg
+            }else{    
+                //Création du msg de la demande de séjour 
+                //msg avec HTML
+                $message = (new Swift_Message('Demande de séjour personalisé'))
+                    ->setFrom($email)
+                    ->setTo('admin@monsite.com')
+                    ->setBody(
+                        $this->renderView('emails/custom-email.html.twig', ['country' => $country,'departureDate' => $departureDate,'returnDate' => $returnDate,'groupNbr' => $groupNbr,'minPrice' => $minPrice,'maxPrice' => $maxPrice,'message' => $message] ),
+                        'text/html'
+                    )
+                    //msg TXT
+                    ->addPart(
+                        $this->renderView('emails/custom-email.txt.twig', ['country' => $country,'departureDate' => $departureDate,'returnDate' => $returnDate,'groupNbr' => $groupNbr,'minPrice' => $minPrice,'maxPrice' => $maxPrice,'message' => $message]),
+                        'text/plain'
+                    )
+                ;
+                //Envoie du msg a l'admin
+                $mailer->send($message);
+                //retour de la page avec msg de succès
+                return $this->render('custom-travel.html.twig', array('success' => true));
+            }
       
     } 
     //page par défaut 
@@ -134,59 +134,59 @@ public function travelList(){
 */
 public function connection(Request $request){
 
-//Si l'admin est déjà connectée, on le redirige vers la page d'ajout de séjour
-$session = $this->get('session');
-if($session->has('account')){
-    return $this->redirectToRoute('travel-design');
-}
-
-//Si le formulaire a été cliqué
-if($request->isMethod('POST')){
-
-    //Recupération des données du formulaire avec l'objet $request
-    $email = $request->request->get('email');
-    $password = $request->request->get('password');
-
-    //bloc des vérifs
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $errors['invalidEmail'] = true;
+    //Si l'admin est déjà connectée, on le redirige vers la page d'ajout de séjour
+    $session = $this->get('session');
+    if($session->has('account')){
+        return $this->redirectToRoute('travel-design');
     }
 
-    if(!preg_match('#^.{8,300}$#', $password)){
-        $errors['invalidPassword'] = true;
-    }
-    
-    //Si pas d'erreurs
-    if(!isset($errors)){
+    //Si le formulaire a été cliqué
+    if($request->isMethod('POST')){
 
-        //Recherche l'admin avec son adresse email dans la BDD
-        $adminRepo = $this->getDoctrine()->getRepository(Admin::class);
-         $admin = $adminRepo->findOneByEmail($email);
-        
-        //Si l'admin a été trouvé, c'est que le compte existe
-         if(!empty($admin)){
+        //Recupération des données du formulaire avec l'objet $request
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
 
-            //Vérif si le mot de passe est bon 
-          if(password_verify($password, $admin->getPassword())){
-
-                //Connexion de l'admin
-                $session->set('account', $admin);
-
-                //Si connexion réussit, charge de la vue avec msg succès
-                return $this->render('connection.html.twig', array('success' => true));
-            
-            //Si mauvais mot de passe alors erreur
-            }else{
-                $errors['badPassword'] = true;
-            }
-        
-        //Si admin n'existe pas alors erreur
-        }else{
-            $errors['notExist'] = true;
+        //bloc des vérifs
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errors['invalidEmail'] = true;
         }
-    }
 
-}
+        if(!preg_match('#^.{8,300}$#', $password)){
+            $errors['invalidPassword'] = true;
+        }
+        
+        //Si pas d'erreurs
+        if(!isset($errors)){
+
+            //Recherche l'admin avec son adresse email dans la BDD
+            $adminRepo = $this->getDoctrine()->getRepository(Admin::class);
+            $admin = $adminRepo->findOneByEmail($email);
+            
+            //Si l'admin a été trouvé, c'est que le compte existe
+            if(!empty($admin)){
+
+                //Vérif si le mot de passe est bon 
+            if(password_verify($password, $admin->getPassword())){
+
+                    //Connexion de l'admin
+                    $session->set('account', $admin);
+
+                    //Si connexion réussit, charge de la vue avec msg succès
+                    return $this->render('connection.html.twig', array('success' => true));
+                
+                //Si mauvais mot de passe alors erreur
+                }else{
+                    $errors['badPassword'] = true;
+                }
+            
+            //Si admin n'existe pas alors erreur
+            }else{
+                $errors['notExist'] = true;
+            }
+        }
+
+    }
 
     //Si erreurs, charge de la vue en lui envoyant ces erreurs en paramètre
     if(isset($errors)){
@@ -263,6 +263,20 @@ public function contact(Request $request, Swift_Mailer $mailer, Recaptcha $recap
                 )
             ;
             $mailer->send($message);
+
+            $messagereceipt = (new Swift_Message('ASIADREAM: Votre mail a bien été receptionné'))
+            ->setFrom('asiadream@gmail.com')
+            ->setTo($email)
+            ->setBody(
+                $this->renderView('emails/receipt-email.html.twig',['content'=>$content, 'subject'=>$subject]),
+                'text/html'
+            )
+            ->addPart(
+                $this->renderView('emails/receipt-email.txt.twig', ['content'=>$content , 'subject'=>$subject]),
+                'text/plain'
+            )
+            ;
+            $mailer->send($messagereceipt);
             //retour de la page contact avec le tableau contenant le message de succès d'envoi de mail
             return $this->render('contact.html.twig', array('success' => true));
         }  
@@ -284,9 +298,10 @@ public function travelDetail(){
 * @Route("/donnez-votre-avis/", name="review")
 * Page du contact du site
 */
-public function review(){
+public function review(Request $request){
 
     return $this->render('review.html.twig');
+
 }
 
 
